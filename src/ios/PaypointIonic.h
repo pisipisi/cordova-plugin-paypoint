f9d2cc7b20fbffb43ca357b43ec33c8ff7303871
Thanh <thanhsnguyen@hotmail.com>
2020-12-18 21:56:40 -0600

    add cocoa

--- src/ios/PaypointIonic.h
+++ src/ios/PaypointIonic.h
@@ -1,6 +1,6 @@
 #import <Cordova/CDV.h>
-#import <iDock/ETPPiDock.h>
-
+// #import <iDock/ETPPiDock.h>
+#import "iDock.framework/Headers/ETPPiDock.h"
 @interface PaypointIonic : CDVPlugin {
   // Member variables go here.
 }
